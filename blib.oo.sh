c4d683a93fb3b0977dbea9b660d55d84e6f67e3b
Cj-bc <cj-bc@y-modify.org>
2018-11-01 08:57:16 +0900

    Change: use realpath to verify path

--- blib.oo.sh
+++ blib.oo.sh
@@ -44,6 +44,7 @@ class:blib() {
   blib::uninstall() {
     [string] libname
 
+    [[ ! "$(realpath $(blib::options --prefix)/${libname})" =~ $(blib::options --prefix)/* ]] && e="invalid library name." throw && return
     [[ "$libname" =~ .*/.* ]] && libname=${libname#*/} # remove username if it's appended
     [[ ! -d "$(blib::options --prefix)/${libname}" ]] && e="library ${libname} is not installed." throw && return
     echo "Removing [${libname}]..."
