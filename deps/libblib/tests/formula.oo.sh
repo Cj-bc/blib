ec1e52d0d4ed30ea13f7430e253e5fd41f78e9d0
Cj-bc <cj-bc@y-modify.org>
2018-11-19 12:41:35 +0900

    Change: libblib> tests> use absolute path for each src

--- deps/libblib/tests/formula.oo.sh
+++ deps/libblib/tests/formula.oo.sh
@@ -6,32 +6,33 @@
 # @(#) version -
 
 
-source "$(cd "${BASH_SOURCE[0]%/*}" && pwd)/../../bash-oo-framework/lib/oo-bootstrap.sh"
+test_path="$(cd "${BASH_SOURCE[0]%/*}" && pwd)"
+source "${test_path}/../../bash-oo-framework/lib/oo-bootstrap.sh"
 import util/test UI/Color util/tryCatch util/log util/exception
 import ../../libblib/formula.oo.sh
 
 describe 'Blib_Formula::validateFormula'
   it 'tests the proper behavior'
   try
-    Blib_Formula::validateFormula 'src/proper_formula'
+    Blib_Formula::validateFormula "${test_path}/src/proper_formula"
   expectPass
 
   it 'tests without initialized Library'
   try
-    Blib_Formula::validdateFormula 'src/uninit_formula'
   expectFail
+    Blib_Formula::validateFormula "${test_path}/src/uninit_formula"
 
   it 'tests without url'
   try
-    Blib_Formula::validdateFormula 'src/nourl_formula'
   expectFail
+    Blib_Formula::validateFormula "${test_path}/src/nourl_formula"
 
   it 'tests without name'
   try
-    Blib_Formula::validdateFormula 'src/noname_formula'
   expectFail
+    Blib_Formula::validateFormula "${test_path}/src/noname_formula"
 
   it 'tests without scripts'
   try
-    Blib_Formula::validdateFormula 'src/noscripts_formula'
   expectFail
+    Blib_Formula::validateFormula "${test_path}/src/noscripts_formula"
