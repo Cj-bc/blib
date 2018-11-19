26757c2d084c70434385dcc89b39f682ed23902c
Cj-bc <cj-bc@y-modify.org>
2018-11-19 18:16:23 +0900

    Change: libblib> tests> add new test handler to test exception

--- deps/libblib/tests/formula.oo.sh
+++ deps/libblib/tests/formula.oo.sh
@@ -11,6 +11,19 @@ source "${test_path}/../../bash-oo-framework/lib/oo-bootstrap.sh"
 import util/test UI/Color util/tryCatch util/log util/exception
 import ../../libblib/formula.oo.sh
 
+# special handler
+function expectException() {
+  caught
+  if [[ "${__EXCEPTION__[1]}" = "$1" ]];then
+    Test EchoedOK
+    Test errors = false
+  else
+    Test Fail "${__EXCEPTION__[@]}"
+  fi
+}
+
+# ------------------------------------
+
 describe 'Blib_Formula::validateFormula'
   it 'tests the proper behavior'
   try
@@ -19,20 +32,26 @@ describe 'Blib_Formula::validateFormula'
 
   it 'tests without initialized Library'
   try
-  expectFail
+      Blib_Formula::validateFormula "hoge"
+  catch expectException "File not exist"
+
+  try
     Blib_Formula::validateFormula "${test_path}/src/uninit_formula"
+  catch expectException "not found [Library]"
 
   it 'tests without url'
   try
-  expectFail
     Blib_Formula::validateFormula "${test_path}/src/nourl_formula"
+  catch expectException "not found [url]"
 
   it 'tests without name'
   try
-  expectFail
     Blib_Formula::validateFormula "${test_path}/src/noname_formula"
+  catch expectException "not found [name]"
 
   it 'tests without scripts'
   try
-  expectFail
     Blib_Formula::validateFormula "${test_path}/src/noscripts_formula"
+  catch expectException "not found [scripts]"
+
+  summary
